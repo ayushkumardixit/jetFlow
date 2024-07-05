6558bdecc699052d907026822e3f6a847becc537
Ayush Dixit <akdixitdeoria@gmail.com>
2024-07-05 15:13:28 +0200

    Add postprocess codes for volume flow rate vs x

--- postProcess/getQ_x_02.c
+++ postProcess/getQ_x_02.c
@@ -12,19 +12,15 @@ int main(int a, char const *arguments[])
     boundary((scalar *){u.x, u.y});
     for (int i = 0; i < n; i++)
     {
-        int nocells = 0;
-        array[0][i] = i + 0.5;
-        foreach ()
+        for (int j = 0; j < n; j++)
         {
-            if (x < (i + 1) && x > i)
-            {
-                array[1][i] +=  2 * pi * y * Delta * u.x[];
-            }
+            array[1][i] += 2 * pi * y * 0.5 * interpolate(u.x, i + 0.5, j + 0.5);
         }
-    }
+        array[0][i] = i + 0.5;
 
-    for (int i = 0; i < n; i++)
-    {
-        fprintf(ferr, "%d %g %g\n", nocells, array[0][i], array[1][i]);
+        for (int i = 0; i < n; i++)
+        {
+            fprintf(ferr, "%d %g %g\n", nocells, array[0][i], array[1][i]);
+        }
     }
 }
