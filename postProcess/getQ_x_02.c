be0d6c6a6079d82a1d677283fcf17d32a2041df3
ayushkumardixit <akdixitdeoria@gmail.com>
2024-07-05 16:21:55 +0200

    Add postprocess volume vs x

--- postProcess/getQ_x_02.c
+++ postProcess/getQ_x_02.c
@@ -1,10 +1,13 @@
 #include "axi.h"
 #include "navier-stokes/centered.h"
 
+#define n 40
+
 char filename[80];
-const int n = 80;
+//const int n = 80;
 double delta_get = 0.5 * 40 / n;
 double array[2][n] = {0};
+double x_get, y_get;
 
 int main(int a, char const *arguments[])
 {
@@ -13,12 +16,13 @@ int main(int a, char const *arguments[])
     boundary((scalar *){u.x, u.y});
     for (int i = 0; i < n; i++)
     {
+	x_get = 40*i/n + delta_get;
+	array[0][i] = x_get;	
         for (int j = 0; j < n; j++)
         {
-            array[1][i] += 2 * pi * (j + delta_get) * interpolate(u.x, i + delta_get, j + delta_get);
-        }
-        array[0][i] = i + delta_get;
-
-        fprintf(ferr, "%.2e %.2e\n", array[0][i], array[1][i]);
+	    y_get = 40*j/n + delta_get;	
+            array[1][i] += 2 * pi * y_get * delta_get * interpolate(u.x, x_get, y_get);
+        }	
+    	fprintf(ferr, "%d %.3e %.2e\n", i, array[0][i], array[1][i]);
     }
 }
